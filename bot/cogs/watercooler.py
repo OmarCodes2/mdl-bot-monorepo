0487987d1ed8f5aeb0834711d4b32de2507c0688
OmarCodes2 <omarcodes2@gmail.com>
2024-05-23 16:27:14 -0400

    use a questions.txt instead of having questions in watercooler.py

--- bot/cogs/watercooler.py
+++ bot/cogs/watercooler.py
@@ -6,14 +6,12 @@ from discord.ext import commands
 class Watercooler(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.topics = [
-            "Would you rather have dinner with Jay-Z or 500 thousand dollars?",
-            "If you could only eat one meal for the rest of your life, what would it be?",
-            "What did you want to be when you grew up when you were younger?",
-            "Would you rather eat a really good mango or a really good watermelon?",
-            "What is the longest you have gone without sleep? (WOOO ENGINEERING!)",
-            "Are ghosts real?"
-        ]
+        self.topics = self.load_topics_from_file("questions.txt")
+
+    def load_topics_from_file(self, file_path):
+        with open(file_path, "r") as file:
+            topics = [line.strip() for line in file if line.strip()]
+        return topics
 
     @commands.command(name='watercooler')
     async def watercooler(self, ctx):
