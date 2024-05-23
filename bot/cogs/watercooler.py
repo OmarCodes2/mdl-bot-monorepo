e2cd96bc271f5d48a450b94719d0b3a97b43595c
OmarCodes2 <omarcodes2@gmail.com>
2024-05-23 16:39:58 -0400

    add custom gif functionality

--- bot/cogs/watercooler.py
+++ bot/cogs/watercooler.py
@@ -2,17 +2,20 @@ import os
 import random
 import discord
 from discord.ext import commands
-
+from giphy_client.rest import ApiException
+import giphy_client
 
 class Watercooler(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.giphy_api_key = os.getenv('GIPHY_API_KEY')
+        if not self.giphy_api_key:
+            raise ValueError("No GIPHY_API_KEY found in environment variables")
+        self.giphy_instance = giphy_client.DefaultApi()
         self.topics = self.load_topics_from_file("questions.txt")
 
     def load_topics_from_file(self, file_name):
-
         script_dir = os.path.dirname(__file__)
-
         file_path = os.path.join(script_dir, file_name)
         with open(file_path, "r") as file:
             topics = [line.strip() for line in file if line.strip()]
@@ -26,14 +29,23 @@ class Watercooler(commands.Cog):
             description=topic,
             color=discord.Color.blue()
         )
-        embed.set_image(
-            url=(
-                "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOGV0c"
-                "2pqb3N5aGpuc3U1NHRsNmRkcXJoamxrMThidHRuNjZvODZ3ciZlcD12MV9p"
-                "bnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/SeysxkSfenHY4/giphy.gif"
-            )
-        )
+
+        gif_url = self.get_gif_url(topic)
+        if gif_url:
+            embed.set_image(url=gif_url)
+        else:
+            embed.set_footer(text="No GIF found for this topic.")
+
         await ctx.send(embed=embed)
+
+    def get_gif_url(self, query):
+        try:
+            response = self.giphy_instance.gifs_search_get(self.giphy_api_key, query, limit=1)
+            if response.data:
+                return response.data[0].images.fixed_height.url
+        except ApiException as e:
+            print(f"Exception when calling Giphy API: {e}")
+        return None
 
 
 async def setup(bot):
