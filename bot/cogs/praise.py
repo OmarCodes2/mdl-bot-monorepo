0f5c676d0b3d2e0f6f2c6611cca6b830f9234029
OmarCodes2 <omarcodes2@gmail.com>
2024-05-24 16:27:15 -0400

    add giphy to praise

--- bot/cogs/praise.py
+++ bot/cogs/praise.py
@@ -1,9 +1,35 @@
 import discord
+import requests
+import random
+import os
 from discord.ext import commands
+
+GIPHY_SEARCH_URL = 'https://api.giphy.com/v1/gifs/search'
 
 class Praise(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    def get_random_clap_gif(self):
+        giphy_api_key = os.getenv('GIPHY_API_KEY')
+        if not giphy_api_key:
+            return None
+
+        params = {
+            'api_key': giphy_api_key,
+            'q': 'clapping',
+            'limit': 50,
+            'offset': 0,
+            'rating': 'G',
+            'lang': 'en'
+        }
+        response = requests.get(GIPHY_SEARCH_URL, params=params)
+        if response.status_code == 200:
+            data = response.json()
+            if data['data']:
+                gif = random.choice(data['data'])
+                return gif['images']['original']['url']
+        return None
 
     @commands.command(name='praise')
     async def praise(self, ctx, *, message: str):
@@ -40,7 +66,11 @@ class Praise(commands.Cog):
                 member_avatar_url = members[0].avatar.url if members[0].avatar else members[0].default_avatar.url
                 embed.set_thumbnail(url=member_avatar_url)
             else:
-                embed.set_thumbnail(url=ctx.guild.icon.url if ctx.guild.icon else None)
+                clap_gif_url = self.get_random_clap_gif()
+                if clap_gif_url:
+                    embed.set_image(url=clap_gif_url)
+                else:
+                    embed.set_thumbnail(url=ctx.guild.icon.url if ctx.guild.icon else None)
 
             await ctx.send(embed=embed)
             await ctx.message.delete()
